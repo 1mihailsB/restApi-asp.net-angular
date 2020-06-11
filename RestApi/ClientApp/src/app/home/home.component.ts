84e575d042aa84008059deee0007e58d8c9b9fe8
Mihails B <1mihails.b@gmail.com>
2020-06-11 18:01:47 +0200

    move tenant, their apartment and house info from main page to 'tenants' component

--- RestApi/ClientApp/src/app/home/home.component.ts
+++ RestApi/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { DatePipe } from '@angular/common'
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -9,44 +8,9 @@ import { DatePipe } from '@angular/common'
 })
 export class HomeComponent {
 
-  public tenants: Tenant[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, public datepipe: DatePipe) {
-    http.get<Tenant[]>(baseUrl + 'api/Tenants').subscribe(result => {
-      this.tenants = result;
-    }, error => console.error(error));
+
+  constructor() {
+
   }
-}
-
-interface Tenant {
-  id: number;
-  firstName: string;
-  lastName: string;
-  personalCode: string;
-  birthday: string;
-  phoneNumber: string;
-  email: string;
-  apartmentId: number | null;
-  apartment: Apartment | null;
-}
-
-interface Apartment {
-  id: number;
-  number: number;
-  floor: number;
-  amountOfRooms: number;
-  amountOfTenants: number;
-  totalArea: number;
-  availableArea: number;
-  houseId: number | null;
-  house: House | null;
-}
-
-interface House {
-  id: number;
-  number: number;
-  street: string;
-  city: string;
-  country: string;
-  postIndex: string;
 }
--- RestApi/ClientApp/src/app/tenants/tenants.component.ts
+++ RestApi/ClientApp/src/app/tenants/tenants.component.ts
@@ -1,14 +1,53 @@
-﻿import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { DatePipe } from '@angular/common'
 
 @Component({
-    selector: 'app-tenants',
-    templateUrl: './tenants.component.html',
-    styleUrls: ['./tenants.component.scss']
+  selector: 'app-tenants',
+  templateUrl: './tenants.component.html',
+  styleUrls: ['./tenants.component.scss']
 })
-/** Tenants component*/
-export class TenantsComponent {
-    /** Tenants ctor */
-    constructor() {
 
-    }
+export class TenantsComponent {
+  public tenants: Tenant[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, public datepipe: DatePipe) {
+    http.get<Tenant[]>(baseUrl + 'api/Tenants').subscribe(result => {
+      this.tenants = result;
+    }, error => console.error(error));
+  }
+
+}
+
+interface Tenant {
+  id: number;
+  firstName: string;
+  lastName: string;
+  personalCode: string;
+  birthday: string;
+  phoneNumber: string;
+  email: string;
+  apartmentId: number | null;
+  apartment: Apartment | null;
+}
+
+interface Apartment {
+  id: number;
+  number: number;
+  floor: number;
+  amountOfRooms: number;
+  amountOfTenants: number;
+  totalArea: number;
+  availableArea: number;
+  houseId: number | null;
+  house: House | null;
+}
+
+interface House {
+  id: number;
+  number: number;
+  street: string;
+  city: string;
+  country: string;
+  postIndex: string;
 }
